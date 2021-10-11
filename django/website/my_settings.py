3bec6b4f1c3cb3b722cc062c7dd1e8e8d10c33a0
Hayeon Kim <85288036+HayeonKimm@users.noreply.github.com>
2021-10-11 16:15:21 +0900

    Update my_settings.py

--- django/website/my_settings.py
+++ django/website/my_settings.py
@@ -3,10 +3,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'finalPJT',
         'USER': 'mulcam',
-        'PASSWORD': 'mulcam',
-        'HOST': '3.37.159.174',
-        'PORT': '3306',
+        'PASSWORD': '*****',
+        'HOST': '*****',
+        'PORT': '****',
     }
 }
 # python manage.py inspectdb 실행 후 출력된 db 찾아서 복사
-SECRET_KEY = 'django-insecure-zkai4llctyqc+z#c-c7w2y+43+2kh$70-6nb^8a+9gsge3898!'
+SECRET_KEY = 'django-insecure-************'
